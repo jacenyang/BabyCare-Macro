e94ca868420f5575a41b740506987a7ebc542d7e
ivanwinata96 <ivanwinata96@users.noreply.github.com>
2020-08-10 10:36:04 +0700

    Initial Commit

---
+++ BabyCare-MC3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BabyCare-MC3
+//
+//  Created by Ivan Winata on 10/08/20.
+//  Copyright © 2020 Ivan Winata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
