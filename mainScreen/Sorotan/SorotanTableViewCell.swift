2b6f70b4f0a382f30cf0ed0874f0419869479106
Zulfa Aliyah <zulfaliyah22@gmail.com>
2020-08-10 13:22:20 +0700

    add label to sorotanCell

---
+++ mainScreen/Sorotan/SorotanTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  SorotanTableViewCell.swift
+//  BabyCare-MC3
+//
+//  Created by Zulfa Aliyah on 10/08/20.
+//  Copyright © 2020 Ivan Winata. All rights reserved.
+//
+
+import UIKit
+
+class SorotanTableViewCell: UITableViewCell {
+
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
