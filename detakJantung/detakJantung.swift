7941a7931d1ae04e6edd69f520c17fdd824f9533
ivanwinata96 <ivanwinata96@users.noreply.github.com>
2020-08-10 10:50:29 +0700

    add storyboard anf grouping files

---
+++ detakJantung/detakJantung.swift
@@ -0,0 +1,30 @@
+//
+//  detakJantung.swift
+//  BabyCare-MC3
+//
+//  Created by Ivan Winata on 10/08/20.
+//  Copyright © 2020 Ivan Winata. All rights reserved.
+//
+
+import UIKit
+
+class detakJantung: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ suhu/suhu.swift
@@ -0,0 +1,30 @@
+//
+//  suhu.swift
+//  BabyCare-MC3
+//
+//  Created by Ivan Winata on 10/08/20.
+//  Copyright © 2020 Ivan Winata. All rights reserved.
+//
+
+import UIKit
+
+class suhu: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
