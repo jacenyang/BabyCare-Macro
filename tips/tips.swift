51d888386eb88897254955c6f572dd24d4368060
riian26 <riianofficial@gmail.com>
2020-08-13 13:33:42 +0700

    Sudah bisa scroll view di Tips.storyboard

--- tips/tips.swift
+++ tips/tips.swift
@@ -10,10 +10,14 @@ import UIKit
 
 class tips: UIViewController {
 
+    @IBOutlet weak var titlelbl: UILabel!
+    @IBOutlet weak var textLbl: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
+        titlelbl.text = "Demam"
+        textLbl.text = "ajhkskdnbajkhdkasdbasjkdbkjabdkjabsdkjbasjkdbaskjdbaksdbaskdbasdkjbdkjbdaskdbkjdsbkjdsabdkjbdkjasbdkjabdkabdkbaksdbakjsdbkajsbdkjabdskabdkabsdkjbasdbajhdbajdbjahbdjasbdjabdjabdjbajdbajsbdajbdjasbjahbdjhabdjhbasdhjabjdhbadasjhbajhbdhjabdjhabsjhbadjhbajdbajdbjhasdbjasbdjabsdjabdvasgkdkgfhfghkdfghkugfhddghsufkghdfkshkdgffdghfdfghdfsghfsgkghkfddfsghkghkffghfgfghfghsdfghjgfhjkdgdfshjfgdhjsghfdfdshghdfgsjfsdghkghfdkfdghksjfdhsgkjhfsdgjkhfgdsjkhgjkfssadfhgjkfhgjkfshgjfsghjkhgjfkfdshgjhfgjdkfdshjgkfhgjkdhgjdkhjgfdksfghksjhgdsfkfsdjghkfhgjskfdshjgkfdshgkfdghjskfsdhgkhgkfdfsdhjgkjfhkdgfjhkdsgfsdhjkgfddfshjgkfdsahjgkfsdhkdsfjghkjdhsfgkjdfhsgkfhdgskhgdfkghkfhjdfgshgkdfhdfgdfsjhkgdsjfhkghdjfgskhgjskdfshjgkdfshjgkhdfjgskfdhsjgkhdfjgskdfhjsgkhjdfghfdo;ihwehewrhewrehwrewrh ewrohi;erhierhiewrhi;rehw;wehr;iwhreherwi;n reh;iehr;iwerhw;erwhi;n erhw;ewrh;n ;hwri nh;erwin wh;rin ;ehirwn ;hrew ewr;h i;hi eoerw;hio erhw ioh;i oen;heirno;hionrerhiwno hirno ehirno rewhino rwehino erwhino erhiwno eoiwhn eiwr hnoerwihno reiohn ewrhin er hinoeior herw hinohio ewwehorhi werh ierwh ionr;hi oeh ioehew iohw eioh; iwoewi hi werhio wehi oewrh ioewh;io h weiowh eirh ioewh ionwheiworh iwe; hoiwerheworwe hiwehio ;hwe ;he ;hiwer h;iewr ;hiwe;hiweor"
         
     }
     
