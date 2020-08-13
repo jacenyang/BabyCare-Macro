dc8dde62015bb9264fbd66f97688580322fb2667
abdulrazak01 <rojakdewa@gmail.com>
2020-08-13 11:56:18 +0700

    Parse JSON , Manggil data API , Nampilkan data API

---
+++ Parse JSON/API.swift
@@ -0,0 +1,22 @@
+//
+//  API.swift
+//  BabyCare-MC3
+//
+//  Created by abdulrazak on 13/08/20.
+//  Copyright © 2020 Ivan Winata. All rights reserved.
+//
+
+import Foundation
+struct ThinkSpeakAPI2: Decodable {
+   
+    let feeds: [feeds2]
+    
+   
+}
+struct feeds2: Decodable {
+     
+      let field1: String?
+      let field2: String?
+    
+    
+  }
--- suhu/suhu.swift
+++ suhu/suhu.swift
@@ -8,11 +8,23 @@
 
 import UIKit
 
-class suhu: UIViewController {
-
+class suhu: UIViewController,UITableViewDelegate,UITableViewDataSource {
+var dataAPI = [feeds2]()
+    let animal = ["cat","dog"]
+    @IBOutlet weak var tblView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tblView.delegate = self
+        tblView.dataSource = self
+        
+        fetchAPIthinkSpeak{
+            DispatchQueue.main.async {
+                self.tblView.reloadData()
+                //print(self.dataAPI)
+               // self.setChartValues()
+            }
+            
+        }
         // Do any additional setup after loading the view.
     }
     
@@ -26,5 +38,61 @@ class suhu: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    func fetchAPIthinkSpeak( onSuccess : @escaping () -> Void){
+        guard let apiURL = URL(string: "https://api.thingspeak.com/channels/1111876/feeds.json?") else{return}
+        URLSession.shared.dataTask(with: apiURL) {(data, response, error) in
+            
+            
+            guard let data = data else {return}
+            
+            do {
+                let decoder = JSONDecoder()
+                let thinkSpeakData = try decoder.decode(ThinkSpeakAPI2.self, from: data)
+                //print(thinkSpeakData.feeds)
+               self.dataAPI = thinkSpeakData.feeds ?? []
+                //print(self.dataAPI)
+                
+                // print(thinkSpeakData.channel?.name)
+                
+                //    print("ini data yang pertama", thinkSpeakData.feeds?.first)
+                //  print("ini data yang terakhir", thinkSpeakData.feeds?.last)
+                DispatchQueue.main.async {
+                   //self.lbltampil.text = (thinkSpeakData.feeds.last?.field1)
+                   // self.lblPulse.text = (thinkSpeakData.feeds.last?.field2)
+                    onSuccess()
+                }
+               
+                
+                
+                
+                
+                
+                
+                
+                
+            } catch let err {
+                print("error", err)
+            }
+        }.resume()
+        
+        
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+           return 1
+       }
+       
+       func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataAPI.count
+       }
+       
+       func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+           
+           let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
+           
+        cell.textLabel?.text = dataAPI[indexPath.row].field1
+           
+           return cell
+       }
+    
 }
