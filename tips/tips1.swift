97f0fe25e21389ce3503221b26ba5f7ee6a4e75b
Zulfa Aliyah <zulfaliyah22@gmail.com>
2020-08-14 20:13:27 +0700

    added asset and tips bradycardia. edited proportional height

--- tips/tips1.swift
+++ tips/tips1.swift
@@ -10,10 +10,32 @@ import UIKit
 
 class tips1: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var pengertianLabel: UILabel!
+    @IBOutlet weak var penyebabLabel: UILabel!
+    @IBOutlet weak var pertolonganLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        showTips()
         // Do any additional setup after loading the view.
+    }
+    
+    func showTips(){
+        titleLabel.text = "Bradycardia"
+        
+        pengertianLabel.text = "Bradycardia merupakan kondisi dimana bayi menunjukkan gejala denyut nadi rendah (<80 bpm) dan disertai nafas lambat."
+        
+        penyebabLabel.text = """
+        1. Kekurangan oksigen
+        2. Temperatur badan bayi rendah
+        3. Efek atau pengaruh obat
+        4. Penyakit jantung bawaan
+"""
+
+        pertolonganLabel.text = """
+        Pastikan bahwa tidak ada kemungkinan jalan nafas bayi terhambat, seperti posisi dada yang tertekan, jalur nafas yang tertutup atau mungkin kondisi ruangan/ambient yang tidak nyaman, misalnya terlalu panas dan lembab. Segera bawa bayi ke dokter dan beri penanganan intensif!
+        """
     }
     
 
