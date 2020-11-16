6b0126e7a0544f4e26c0fd35cfcd59e6288df24c
braannn <65325830+braannn@users.noreply.github.com>
2020-11-16 08:39:46 +0700

    add temp code to screen kondisi bayi and kondisi cabin

--- mainScreen/KondisiBayiVC.swift
+++ mainScreen/KondisiBayiVC.swift
@@ -8,13 +8,34 @@
 
 import UIKit
 
-class KondisiBayiVC: ViewController {
+class KondisiBayiVC: ViewController, SensorManagerDelegate {
+    
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    
+    @IBOutlet weak var babyTempLabel: UILabel!
+    @IBOutlet weak var pulseLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    
+    var sensorManager = SensorManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         navigationItem.largeTitleDisplayMode = .never
+        
+        sensorManager.delegate = self
+        sensorManager.fetchSensor()
+    }
+    func didFailWithError(error: Error) {
+        print(error)
     }
     
-    
+    func didUpdateSensor(_ sensorManager: SensorManager, sensor: SensorModel) {
+        DispatchQueue.main.async {
+            
+            self.pulseLabel.text = sensor.pulseString
+            self.babyTempLabel.text = sensor.babyTempString
+            self.weightLabel.text = sensor.weightToKgString
+        }
+    }
 }
