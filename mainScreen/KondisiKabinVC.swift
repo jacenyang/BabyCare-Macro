f92f1616832fa1f4437b0065e48721d61f07fcba
braannn <65325830+braannn@users.noreply.github.com>
2020-11-16 08:40:28 +0700

    add new kondisikabin view controller for konsisi kabin screen list

---
+++ mainScreen/KondisiKabinVC.swift
@@ -0,0 +1,39 @@
+//
+//  KondisiKabinVC.swift
+//  BabyCare-MC3
+//
+//  Created by Muhammad Syabran on 16/11/20.
+//  Copyright © 2020 Ivan Winata. All rights reserved.
+//
+
+import UIKit
+
+class KondisiKabinVC: ViewController, SensorManagerDelegate {
+    
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    
+    @IBOutlet weak var cabinTempLabel: UILabel!
+    @IBOutlet weak var humidityLabel: UILabel!
+    
+    var sensorManager = SensorManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.largeTitleDisplayMode = .never
+        
+        
+        sensorManager.delegate = self
+        sensorManager.fetchSensor()
+    }
+    func didFailWithError(error: Error) {
+        print(error)
+    }
+    
+    func didUpdateSensor(_ sensorManager: SensorManager, sensor: SensorModel) {
+        DispatchQueue.main.async {
+            
+            self.cabinTempLabel.text = sensor.cabinTempString
+            self.humidityLabel.text = sensor.humidityString
+        }
+    }
+}
