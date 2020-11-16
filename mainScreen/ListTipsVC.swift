bd27715bfbc8c29e2897b2507c9aaa571ece6f24
braannn <65325830+braannn@users.noreply.github.com>
2020-11-16 08:22:13 +0700

    improve list tips screen with scroll view

---
+++ mainScreen/ListTipsVC.swift
@@ -0,0 +1,19 @@
+//
+//  ListTipsVC.swift
+//  BabyCare-MC3
+//
+//  Created by Muhammad Syabran on 16/11/20.
+//  Copyright © 2020 Ivan Winata. All rights reserved.
+//
+
+import UIKit
+
+class ListTipsVC: ViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        
+    }
+}
